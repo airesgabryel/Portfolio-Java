fa3c22fbd68cb981def6d2e1bfebd43379960cf0
airesgabryel <56Gabryel@gmail.com>
2025-10-08 21:18:19 -0300

    first commit

---
+++ HoraDoSistema/src/main/java/com/mycompany/horadosistema/HoraDoSistema.java
@@ -0,0 +1,17 @@
+package com.mycompany.horadosistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Gabry
+ */
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        
+        Date relogio = new Date();
+        System.out.println("Hora do sistema: ");
+        System.out.println(relogio.toString());
+    }
+}
---
+++ IdiomaSistema/src/main/java/com/mycompany/idiomasistema/IdiomaSistema.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.idiomasistema;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author Gabry
+ */
+public class IdiomaSistema {
+
+    public static void main(String[] args) {
+        Locale idiomaSistema = Locale.getDefault();
+        System.out.println("Idioma do sistema: " + idiomaSistema.toString());
+
+    }
+}
