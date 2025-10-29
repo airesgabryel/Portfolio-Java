c541b80dc5f7cd0da45c612ded2e6e7b2e839c65
alvesadriana <adriananascimentoalves6@gmail.com>
2025-10-29 00:56:37 -0300

    Criando projeto de filmes

--- PrimeiroPrograma/src/main/java/com/mycompany/primeiroprograma/PrimeiroPrograma.java
+++ PrimeiroPrograma/src/main/java/com/mycompany/primeiroprograma/PrimeiroPrograma.java
@@ -7,6 +7,14 @@ package com.mycompany.primeiroprograma;
 public class PrimeiroPrograma {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Esse é o Screen Match ");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: "  + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+
+        double notaDoFilme = 8.1;
+        System.out.println(notaDoFilme);
     }
 }
